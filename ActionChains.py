c7435767e0782740d4b476fc0eb92475c503c3b7
Fateme Kharazmi <73032767+faanid@users.noreply.github.com>
2023-05-19 20:13:17 +0430

    action click per second

--- ActionChains.py
+++ ActionChains.py
@@ -1,1 +1,28 @@
 
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+
+PATH = "C\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+driver.get("https://orteil.dashnet.org/cookieclicker/")
+
+driver.implicitly_wait(5)
+
+cookie = driver.find_element_by_id("bigCookie")
+cookie_count = driver.find_element_y_id("cookies")
+items = [driver.find_element_by_id("productPrice" + str(i)) for i in range(1,-1,-1)]
+
+actions = ActionChains(driver)
+actions.click(cookie)
+
+for i in range(5000):
+    actions.perform()
+    count = int(cookie_count.text.split(" ")[0])
+    print(count)
+    for item in items:
+        value = int(item.text)
+        if value <= count:
+            upgrade_actions = ActionChains(driver)
+            upgrade_actions.move_to_element(item)
+            upgrade_actions.click()
+            upgrade_actions.perform()
