90ade5607ba05a492cfd7e3898265b5abed2570f
Fateme Kharazmi <73032767+faanid@users.noreply.github.com>
2023-05-22 23:22:46 +0430

    UnitTest framework 2/2

--- testcase/main.py
+++ testcase/main.py
@@ -14,8 +14,14 @@ class PythonOrgSearch(unittest.TestCase):
     #     assert False
         
     def test_title(self):
-        mainPage = page.MainPage()
+        mainPage = page.MainPage(self.driver)
         assert mainPage.is_title_matches()
+        mainPage.search_text_element = "pycon"
+        mainPage.click_go_button()
+        search_result_page = page.SearchResultPage(self.driver)
+        assert search_result_page.is_results_found()
+        
+   
     
     def tearDown(self):
         self.driver.close()
